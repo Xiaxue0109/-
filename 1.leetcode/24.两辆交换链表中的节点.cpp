0a3d4d780b4c0715607067adf33600a3031f3ed0
Xiaxue0109 <1915223862@qq.com>
2020-02-26 17:40:47 +0800

    24.两两交换链表节点

---
+++ 1.leetcode/24.两辆交换链表中的节点.cpp
@@ -0,0 +1,20 @@
+/*************************************************************************
+	> File Name: 24.两辆交换链表中的节点.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年02月26日 星期三 17时37分59秒
+ ************************************************************************/
+struct ListNode* swapPairs(struct ListNode* head){
+    struct ListNode ret, *p, *q;
+    ret.next = head;
+    p = &ret;
+    q = head;
+    while (q && q->next) {
+        p->next = q->next;
+        q->next = p->next->next;
+        p->next->next = q;
+        p = q;
+        q = q->next;
+    }
+    return ret.next;
+}
