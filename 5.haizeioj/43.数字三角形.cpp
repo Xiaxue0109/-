a3051baa1167bf284f9e2eeb593d237037c0e217
Xiaxue0109 <1915223862@qq.com>
2020-05-14 15:11:04 +0800

    动态规划

--- 5.haizeioj/43.数字三角形.cpp
+++ 5.haizeioj/43.数字三角形.cpp
@@ -17,6 +17,7 @@ int main() {
             cin >> num[i][j];
         }
     }
+    //两个数组，num是存放输入数据的，ans是存放答案的
     for (int i = n; i >= 1; i--) {
         for (int j = 1; j <= i; j++) {
             ans[i][j] = max(ans[i + 1][j + 1], ans[i + 1][j]) + num[i][j];
